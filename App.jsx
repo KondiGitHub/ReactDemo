a005bf2909b413fbf3bd2e22c5d3bf1365b4cec9
KondiGitHub <haichary@yahoo.co.in>
2018-06-26 19:53:00 -0400

    Props Example code1

--- App.jsx
+++ App.jsx
@@ -1,59 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
         this.state = {
-            data:
-                [
-                    {
-                        "id":1,
-                        "name":"Foo",
-                        "age":"20"
-                    },
-                    {
-                        "id":2,
-                        "name":"Bar",
-                        "age":"30"
-                    },
-                    {
-                        "id":3,
-                        "name":"Baz",
-                        "age":"40"
-                    }
-                ]
+            header: "Header from state...",
+            content: "Content from state..."
         }
     }
     render() {
         return (
             <div>
-                <Header/>
-                    {this.state.data.map((person, i) => <Content key = {i} data = {person} />)}
+                <h1>{this.state.header}</h1>
+                <h2>{this.state.content}</h2>
             </div>
-        );
-    }
-}
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
-            </div>
-        );
-    }
-}
-class Content extends React.Component {
-    render() {
-        return (
-            <table>
-                <tbody>
-                <tr>
-                    <td>{this.props.data.id}</td>
-                    <td>{this.props.data.name}</td>
-                    <td>{this.props.data.age}</td>
-                </tr>
-                </tbody>
-            </table>
         );
     }
 }
